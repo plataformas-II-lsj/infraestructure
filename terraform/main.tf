ac6cf4337991b113cbaa9a67ed15cbde039f5702
Santiago0212004 <sjbs0212@gmail.com>
2025-04-20 20:27:24 -0500

    add cluster autoscaling

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,9 +10,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = lower(var.aks_cluster_name)
 
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+    name                = "default"
+    vm_size             = "Standard_DS2_v3"
+    auto_scaling_enabled = true
+    min_count           = 1
+    max_count           = 3
   }
 
   identity {
